38be7f48e9655a3d2c98a1e1344633eba139341c
isharghura <ishar.ghuras@gmail.com>
2024-03-02 13:44:49 -0500

    fetch nearest charging stations

---
+++ evicient/evpoints.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const lat = 45.424159;
+const long = -75.686951;;
+const radius = 5;
+const token = process.env.api_key;
+
+const url = `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${token}&country=CA&latitude=${lat}&longitude=${long}&radius=${radius}&fuel_type=ELEC&limit=1`;
+// const url = `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${token}&latitude=${lat}&longitude=${long}&radius=${radius}&fuel_type=ELEC&limit=1`;
+axios.get(url)
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error(error.response.data);
+    });
