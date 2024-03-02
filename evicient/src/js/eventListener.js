42628869c1462b765d0a67ffa1cf7359a6997844
Yufeng Liu <yufeng.liu15@gmail.com>
2024-03-02 10:12:11 -0500

    added get location function

---
+++ evicient/src/js/eventListener.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //add mouse down listener to our canvas object
+    document.getElementById('get_location').addEventListener('click', getLocation)
+
+  })
---
+++ evicient/src/js/location.js
@@ -0,0 +1,20 @@
+function getLocation()
+{
+    if("geolocation" in navigator)
+    {
+        navigator.geolocation.getCurrentPosition(function(position)
+        {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            var coordinatesElement = document.getElementById("coordinates");
+            coordinatesElement.textContent = "Latitude: " + latitude + ", Longitude " + longitude;
+        }, function(error)
+        {
+            console.error("Error getting location:", error.message);
+        });
+    }
+    else
+    {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
