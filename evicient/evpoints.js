e90fc1c8425c142535f3e6f9d7ccd02b186659c6
isharghura <ishar.ghuras@gmail.com>
2024-03-02 14:09:03 -0500

    removed limit

--- evicient/evpoints.js
+++ evicient/evpoints.js
@@ -7,8 +7,8 @@ const long = -75.686951;;
 const radius = 5;
 const token = process.env.api_key;
 
-const url = `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${token}&country=CA&latitude=${lat}&longitude=${long}&radius=${radius}&fuel_type=ELEC&limit=1`;
-// const url = `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${token}&latitude=${lat}&longitude=${long}&radius=${radius}&fuel_type=ELEC&limit=1`;
+const url = `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${token}&country=CA&latitude=${lat}&longitude=${long}&radius=${radius}&fuel_type=ELEC`;
+
 axios.get(url)
     .then(response => {
         console.log(response.data);
