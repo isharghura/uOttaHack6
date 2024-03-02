630b500ddb11a9fbbc583030bd214040046265d5
peterjinweigu <petergujinwei@gmail.com>
2024-03-02 13:39:29 -0500

    more setup files

--- evicient/src/App.jsx
+++ evicient/src/App.jsx
@@ -1,5 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import login from "./components/login";
+import login from "./components/home";
+import "./styles.css";
+
 function App() {
-  return <div id = "backgrad"></div>
+  return (
+    <Router>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
-export default App
+export default App;
